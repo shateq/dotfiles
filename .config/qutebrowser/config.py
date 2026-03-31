0745eccb2a08143133d60760be373186c8188cd8
Szymon Wieczorek <shateq21@gmail.com>
2026-03-31 21:00:26 +0200

    unimportant garbage

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,11 +1,13 @@
 config.load_autoconfig(False)
 
+c.colors.webpage.preferred_color_scheme = 'dark'
+
 c.completion.shrink = True
 c.completion.web_history.max_items = 6
 c.completion.open_categories = ["quickmarks", "bookmarks", "history"] 
 
 c.confirm_quit = ["downloads"]
-c.editor.command = ["alacritty", "-e", "nvim", "+call cursor ({line}, {column})", "{file}"]
+c.editor.command = ["$TERMINAL", "-e", "nvim", "+call cursor ({line}, {column})", "{file}"]
 
 c.content.pdfjs = True
 c.content.dns_prefetch = False
@@ -15,38 +17,34 @@ c.content.geolocation = False
 c.content.default_encoding = 'utf-8'
 
 c.scrolling.bar = 'always'
-c.colors.webpage.preferred_color_scheme = 'dark'
 c.tabs.show = 'multiple'
 
+## url
+c.url.default_page = "qute://start"
+c.url.searchengines = {
+  "DEFAULT": "https://duckduckgo.com?q={}",
+  "!aw":     "https://wiki.archlinux.org/?search={}",
+  "!ddg":    "https://duckduckgo.com?q={}",
+  "!dl":     "https://deepl.com/translator#_/en/{}",
+  "!diki":   "https://www.diki.pl/slownik-angielskiego?q={}",  
+  "!g" :     "https://www.google.com/search?q={}",
+  "!gh":     "https://github.com/search?q={}",
+  "!h":      "https://hoogle.haskell.org/?hoogle={}",
+  "!lb":     "https://letterboxd.com/search/{}",
+  "!r":      "https://www.reddit.com/search?q={}",
+  "!yt":     "https://www.youtube.com/results?search_query={}",
+  "!w":      "https://wikipedia.org/w/index.php?search={}",
+}
+
 ## binds
-config.bind('<Ctrl-j>', 'completion-item-focus next', mode='command')
-config.bind('<Ctrl-k>', 'completion-item-focus prev', mode='command')
-config.unbind('<Ctrl+h>') #homepage key
+config.bind('<Ctrl-n>', 'completion-item-focus next', mode='command')
+config.bind('<Ctrl-p>', 'completion-item-focus prev', mode='command')
 
 config.bind(';iy', 'hint images yank')
 config.bind(';iY', 'hint images yank-primary')
 config.bind(';iO', 'hint images run open -t {hint-url}')
 
-
 config.bind(',au', 'adblock-update')
 config.bind(',dj', ":set content.javascript.enabled false")
-# example: config.bind(',hw', "jseval alert('Hello World')")
 #config.bind(',ym', 'yank inline [{title}]({url:pretty})')
 
-## url
-c.url.default_page = "qute://start"
-
-c.url.searchengines = {
-  "DEFAULT": "https://duckduckgo.com?q={}",
-  "aw":      "https://wiki.archlinux.org/?search={}",
-  "wp":      "https://wikipedia.org/w/index.php?search={}",
-  "lb":      "https://letterboxd.com/search/{}",
-  "g" :      "https://www.google.com/search?q={}",
-  "ddg":     "https://duckduckgo.com?q={}",
-  "sp":      "https://startpage.com/sp/search?query={}",
-  "dl":      "https://deepl.com/translator#_/en/{}",
-  "diki":    "https://www.diki.pl/slownik-angielskiego?q={}",  
-  "gh":      "https://github.com/search?q={}",
-  "ho":      "https://hoogle.haskell.org/?hoogle={}",
-  "yt":      "https://www.youtube.com/results?search_query={}"
-}
